941ca4a709e77ed2b61787afed9119f097ce97ab
Exxirr <alexjair584@gmail.com>
2023-07-28 10:09:57 -0500

    Calculadora de Propinas

---
+++ Ejercicio/AdivinarNumeros.java
@@ -0,0 +1,27 @@
+package Ejercicio;
+
+import java.util.Scanner;
+
+public class AdivinarNumeros {
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+
+    int numero = (int) (Math.random() * 100);
+
+    System.out.println("Adivina el numero aleatorio : ");
+
+    int adivina = entrada.nextInt();
+
+    System.out.println("Numero es : " + numero);
+
+    if(adivina == numero){
+      System.out.println("Lo lograste");
+    }else{
+      System.out.println("Para la proxima");
+    }
+
+
+
+  }
+}
