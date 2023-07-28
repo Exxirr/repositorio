9fdf341f454328f899e14ed033fbf0719086fb74
Exxirr <alexjair584@gmail.com>
2023-07-28 18:01:17 -0500

    Buscador de paises

---
+++ Ejercicio/BuscadorPaises.java
@@ -0,0 +1,18 @@
+package Ejercicio;
+
+import java.util.Scanner;
+
+public class BuscadorPaises {
+
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println("Ingrese nombre de un pais");
+
+    String pais = entrada.nextLine();
+
+    System.out.println("https://www.google.com/maps/search/" + pais);
+
+  }
+}
