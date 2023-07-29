dd8e2c03a6422ebee3b26b58979a1ef115fa86b3
Exxirr <alexjair584@gmail.com>
2023-07-28 19:34:31 -0500

    Contador del 1 Al 100

---
+++ Ejercicio/Contador1Al100.java
@@ -0,0 +1,18 @@
+package Ejercicio;
+
+
+
+public class Contador1Al100 {
+
+  public static void main(String[] args) {
+
+    int contador = 0;
+
+    while(contador <= 100){
+      System.out.println(contador);
+      contador++;
+    }
+
+
+  }
+}
